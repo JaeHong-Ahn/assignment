e34c36054750db2e4e871d29b79bf4859182a7b2
JaeHongAhn <woghd387@kyonggi.ac.kr>
2024-04-22 20:46:36 +0900

    feat: User 엔티티 추가
    BaseTimeEntitiy를 통해 생성 시각과 최종 수정 시각을 관리

---
+++ src/main/java/com/example/thecommerce/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.thecommerce.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    /**
+     * 회원 가입
+     * 닉네임 중복 확인
+     * identifier 중복 확인
+     * 회원 목록 조회
+     * 회원 정보 수정
+     * 회원 비밀번호 수정
+     * 회원 탈퇴
+     */
+}
---
+++ src/main/java/com/example/thecommerce/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.thecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Component
+@DynamicUpdate
+public class User extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "identifier", unique = true, nullable = false)
+    private String identifier;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "phoneNum", nullable = false)
+    private String phoneNum;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+}
