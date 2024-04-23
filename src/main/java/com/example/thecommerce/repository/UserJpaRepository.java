9f7afc528fde9ad06bcaefbd8eace90f46c5a7dc
JaeHongAhn <woghd387@kyonggi.ac.kr>
2024-04-24 05:46:56 +0900

    fix
    BaseTimeEntity에서 각 엔티티의 수정 시각을 반영하기 위해 어노테이션 추가

--- src/main/java/com/example/thecommerce/repository/UserJpaRepository.java
+++ src/main/java/com/example/thecommerce/repository/UserJpaRepository.java
@@ -3,13 +3,9 @@ package com.example.thecommerce.repository;
 import com.example.thecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
     User findUserByIdentifier(String identifier);
 
     Boolean existsUserByIdentifier(String identifier);
-
-    void deleteUserById(Long id);
 }
