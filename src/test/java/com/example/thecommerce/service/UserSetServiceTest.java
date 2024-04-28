74239a69d4406fda5b14529599eb07c2c35a7530
JaeHongAhn <woghd387@kyonggi.ac.kr>
2024-04-29 02:21:29 +0900

    test: UserSetService에 대한 테스트 코드
    회원가입, 회원 정보 변경, 비밀번호 변경, 회원 탈퇴

--- src/test/java/com/example/thecommerce/service/UserSetServiceTest.java
+++ src/test/java/com/example/thecommerce/service/UserSetServiceTest.java
@@ -1,50 +1,88 @@
 package com.example.thecommerce.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
+import com.example.thecommerce.dto.UserRegisterForm;
+import com.example.thecommerce.dto.UserUpdateForm;
 import com.example.thecommerce.dto.UserUpdatePasswordForm;
+import com.example.thecommerce.dto.UserUpdateResponseDto;
 import com.example.thecommerce.entity.User;
-import com.example.thecommerce.repository.interfaces.UserJpaRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
 class UserSetServiceTest {
 
-    @Mock
-    private UserJpaRepository userJpaRepository;
-
-    @Mock
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Mock
+    @Autowired
     private UserSetService userSetService;
 
+    UserRegisterForm userRegisterForm1 = new UserRegisterForm("woghd387","rkwhr387@","재홍브로", "안재홍", "01025865555", "woghd387@naver.com");
+
+    UserUpdateForm userUpdateForm = new UserUpdateForm("woghd3872", "재홍맨", "안재홍", "01025555555", "woghd387@thecommerce.com");
+
+    UserUpdatePasswordForm userUpdatePasswordForm = new UserUpdatePasswordForm("rkwhr387@", "rkwhr387@");
+
     @Test
-    void updateUserPassword() {
-        // given
-        Long userId = 1L;
-        String pw = "rkwhr387@";
-        User user = new User(userId, "woghd387", passwordEncoder.encode(pw), "재홍브로",
-                "안재홍", "01025865555", "woghd387@naver.com", false);
-        when(userJpaRepository.save(user)).thenReturn(user);
-
-        String newPassword = "rkwhr3872@";
-        String encodedNewPassword = passwordEncoder.encode(newPassword);
-        when(passwordEncoder.encode(newPassword)).thenReturn(encodedNewPassword);
-
-        // when
-        UserUpdatePasswordForm form = new UserUpdatePasswordForm(newPassword, newPassword);
-
-        userSetService.updateUserPassword(userId, form);
+    @DisplayName("회원가입")
+    void createUser() {
+        // given & when
+        User saved = userSetService.createUser(userRegisterForm1);
 
         // then
-        System.out.println(encodedNewPassword);
-        assertTrue(passwordEncoder.matches(encodedNewPassword, user.getPassword()));
+        assertTrue(saved.getIdentifier().equals(userRegisterForm1.getIdentifier()));
     }
 
+    @Test
+    @DisplayName("회원정보변경")
+    void updateUserInfo() {
+        // given
+        User saved = userSetService.createUser(userRegisterForm1);
+
+        //when
+        UserUpdateResponseDto dto = userSetService.updateUserInfo(userUpdateForm, saved.getIdentifier(), saved.getId());
+
+        // then
+        assertEquals("woghd3872", dto.getIdentifier());
+        assertEquals("재홍맨", dto.getNickname());
+        assertEquals("안재홍", dto.getName());
+        assertEquals("01025555555", dto.getPhoneNum());
+        assertEquals("woghd387@thecommerce.com", dto.getEmail());
+
+    }
+
+    @Test
+    @DisplayName("비밀번호변경")
+    void updateUserPassword() {
+        // given
+        User saved = userSetService.createUser(userRegisterForm1);
+
+        // when
+        userSetService.updateUserPassword(saved.getId(), userUpdatePasswordForm);
+
+        // then
+        assertTrue(passwordEncoder.matches("rkwhr387@", saved.getPassword()));
+    }
+
+    @Test
+    @DisplayName("회원탈퇴")
+    void withdrawal() {
+        // given
+        User saved = userSetService.createUser(userRegisterForm1);
+
+        // when
+        userSetService.withdrawal(saved.getId());
+
+        // then
+        assertTrue(saved.getIsDeleted());
+    }
 }
